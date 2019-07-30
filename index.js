dfe9e2aedf6b3506f93cb1dec692a6ecc59c6254
James Sharpe <jameswrightsharpe@gmail.com>
2019-07-30 22:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   var reply ="";
-
   
-  
-  if (string.toLowerCase === string) {reply = "I can\'t hear you!"}
-  if (string.toUpperCase === string) {reply = "YES INDEED!"}
-  if (string === "I love you, Grandma") {reply = "I love you, too."}
+  if (string.toLowerCase() === string) {reply = "I can\'t hear you!"}
+  if (string.toUpperCase() === string) {reply = "YES INDEED!"}
+  if (string === "I love you, Grandma.") {reply = "I love you, too."}
   return reply
 
 }
